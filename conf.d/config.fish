ec4d8af379354815d4ed93545bcd264ee11978fd
Alexander Grigoriev <grelkin@gmail.com>
2020-10-18 09:39:12 +0200

    feat: load pyenv

--- conf.d/config.fish
+++ conf.d/config.fish
@@ -6,6 +6,7 @@ set -x GOPATH "$HOME/go"
 # pyenv
 set -Ux PYENV_ROOT $HOME/.pyenv
 set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+status --is-interactive; and source (pyenv init -|psub)
 
 # aliases
 alias ls='exa --group-directories-first'
