fea39eb37306419438db6ba1e0538635aeb74fcc
Alexander Grigoriev <grelkin@gmail.com>
2020-10-18 09:30:59 +0200

    feat: display last exit code and jobs indicator

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,4 +1,5 @@
 function fish_prompt --description 'Write out the prompt'
+	set -g RETVAL $status
 
 	# Just calculate these once, to save a few cycles when displaying the prompt
 	if not set -q __fish_prompt_hostname
@@ -29,8 +30,16 @@ function fish_prompt --description 'Write out the prompt'
 			set -g __fish_prompt_cwd (set_color $fish_color_cwd)
 		end
 
+		set -l code ''
+		set -l jobs ''
+		if [ $RETVAL -ne 0 ]
+			set code '✘'
+		end
+		if [ (jobs -l | wc -l) -gt 0 ]
+			set jobs '⚙'
+		end
 		set_color 777
-		echo -n -s "[" (jobs | wc -l) "] "
+		echo -n -s "[$code$jobs] "
 		set_color 9F9
 		# echo -n -s "$USER" " "
 		echo -n -s (date "+%H:%M") " "
