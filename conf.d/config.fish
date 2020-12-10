718fac1f830ee2117856dac2a0bc0c8969bca840
Alexander Grigoriev <grelkin@gmail.com>
2020-12-10 08:47:39 +0100

    fix: explicitly set user paths

--- conf.d/config.fish
+++ conf.d/config.fish
@@ -5,7 +5,7 @@ set -x GOPATH "$HOME/go"
 
 # pyenv
 set -Ux PYENV_ROOT $HOME/.pyenv
-set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+set -Ux fish_user_paths $PYENV_ROOT/bin
 status --is-interactive; and source (pyenv init -|psub)
 
 # aliases
