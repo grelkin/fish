ace0e10f9e0e661b8ae53bf4e9fe184a1bf2e552
Alexander Grigoriev <grelkin@gmail.com>
2023-04-02 14:07:41 +0200

    chore: remove obsolete configuration

--- conf.d/config.fish
+++ conf.d/config.fish
@@ -1,13 +1,6 @@
 set EDITOR vi
 set fish_greeting ""
 
-set -x GOPATH "$HOME/go"
-
-# pyenv
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -Ux fish_user_paths $PYENV_ROOT/bin
-status --is-interactive; and source (pyenv init -|psub)
-
 # aliases
 alias ls='exa --group-directories-first'
 alias g='git'
